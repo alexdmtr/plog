bfb83e0d0c43f0cd5a45d6cd0b17f6c8b20dbb3a
Alex Dumitru <alex.dumitru@student.manchester.ac.uk>
2017-11-27 13:51:50 +0000

    Remove default PORT of 3000

--- gulpfile.js
+++ gulpfile.js
@@ -22,9 +22,6 @@ gulp.task('default', function () {
     nodemon({
         script: 'server.js',
         ext: 'js',
-        env: {
-            PORT: 3000
-        },
         ignore: ['./node_modules/**'],
         // tasks: ['test'],
         quiet: true
